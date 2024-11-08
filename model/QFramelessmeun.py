13f3293771bba4204d2846f447ac4741761f53c1
youyo <youyou161abcd@gmail.com>
2024-11-08 14:35:40 +0800

    QAQ

--- model/QFramelessmeun.py
+++ model/QFramelessmeun.py
@@ -1,30 +1,28 @@
 from PyQt6 import QtWidgets, QtCore, QtGui
-from PyQt6.QtCore import QVariantAnimation, pyqtSlot, QVariant, QAbstractAnimation
-from PyQt6.QtGui import QIcon, QPixmap, QTransform
-
-
-class __Struct:
-    def __int__(self, name:str=None, avatar:str=None, ipv4:str=None, ipv6:str=None):
-        self.__name = name
-        self.__avatar = avatar
-        self.__ipv4 = ipv4
-        self.__ipv6 = ipv6
+from PyQt6.QtCore import QPropertyAnimation, pyqtProperty, QAbstractAnimation, QVariantAnimation
+from PyQt6.QtGui import QPixmap, QTransform
 
 
 class QTitlebar(QtWidgets.QWidget):
-    def __init__(self, parent=None, title:str=None):
+    def __init__(self, parent=None, title: str = None):
         super().__init__(parent=parent)
         self.__title = title
-        self.__angle = [0, 180]
+        self._angle = [0, 180]  # 初始化角度
+        self.iconpixmap = QPixmap("D:/python/CatChat/res/down.png")  # 加載圖片
 
+        # 設置動畫
+        self.animation = QVariantAnimation(self)
+
+        # 設置界面
         self.resize(250, 40)
         self.topbarHlay = QtWidgets.QHBoxLayout(self)
         self.titleLable = QtWidgets.QLabel(self)
         self.iconlable = QtWidgets.QLabel(self)
-        self.iconpixmap = QPixmap()
+
         self.ui_init()
 
     def ui_init(self):
+        # 設置標題
         self.topbarHlay.addWidget(self.titleLable, alignment=QtCore.Qt.AlignmentFlag.AlignLeft)
         font = QtGui.QFont()
         font.setFamily('Arial Black')
@@ -38,34 +36,39 @@ class QTitlebar(QtWidgets.QWidget):
         }
         """)
         self.titleLable.setFont(font)
+
+        # 設置 iconlable 大小
         self.iconlable.setFixedSize(15, 15)
         self.iconlable.setAlignment(QtCore.Qt.AlignmentFlag.AlignTop)
-        self.iconpixmap.load("D:/python/CatChat/res/down.png")
-        self.iconlable.setPixmap(self.iconpixmap)
-        self.iconlable.setScaledContents(True)
         self.topbarHlay.addWidget(self.iconlable, alignment=QtCore.Qt.AlignmentFlag.AlignRight)
 
-    def mousePressEvent(self, a0):
-        animation = QVariantAnimation(self.iconlable)
-        animation.setDuration(300)
-        animation.setStartValue(self.__angle[0])
-        animation.setEndValue(self.__angle[1])
-        animation.valueChanged.connect(self.angle)
-        animation.start()
+        # 初始化顯示圖片
+        self.update_icon(0)
 
-    @pyqtSlot(QVariant)
-    def angle(self, value: float) -> None:
-        self.__angle[0] = value
-        self.__angle[1] = value+180
-        trans = QTransform()
-        trans.rotate(value)
-        self.iconlable.setPixmap(self.iconpixmap.transformed(trans))
+    def update_icon(self, angle):
+        self._angle[0] = angle
+        self._angle[1] = angle + 180
+        # 根據當前角度旋轉圖片並更新 QLabel 顯示
+        trans = QTransform().rotate(angle)
+        rotated_pixmap = self.iconpixmap.transformed(trans)
+        self.iconlable.setPixmap(rotated_pixmap)
+        self.iconlable.setScaledContents(True)
 
-class QFramelessmeun(QtWidgets.QWidget):
-    def __init__(self, parent=None, title:str=None):
+    def mousePressEvent(self, event):
+        if self.animation.state() == QAbstractAnimation.State.Running:
+            pass
+        else:
+            self.animation.setDuration(500)  # 設置動畫持續時間
+            self.animation.setStartValue(self._angle[0])
+            self.animation.setEndValue(self._angle[1])
+            self.animation.valueChanged.connect(self.update_icon)  # 每次值改變時更新圖標
+            self.animation.start()
+
+class QFramelessmenu(QtWidgets.QWidget):
+    def __init__(self, parent=None, title: str = None):
         super().__init__(parent=parent)
         self.__title = title
-        self.__list = []
+        self.__datalist = []
 
         self.Vlayout = QtWidgets.QVBoxLayout(self)
         self.__topbar = QTitlebar(parent=self, title=self.__title)
@@ -79,6 +82,3 @@ class QFramelessmeun(QtWidgets.QWidget):
 
         self.__topbar.resize(250, 40)
         self.Vlayout.addWidget(self.__topbar, alignment=QtCore.Qt.AlignmentFlag.AlignTop)
-
-    def getlen(self) -> int:
-        return len(self.__list)
