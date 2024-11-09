ba3c5f360d9e43dcdec37d6d68bcdb63beb21601
youyo <youyou161abcd@gmail.com>
2024-11-09 18:56:12 +0800

    QAQ

--- model/QFramelessmeun.py
+++ model/QFramelessmeun.py
@@ -3,6 +3,12 @@ from PyQt6.QtCore import QAbstractAnimation, QVariantAnimation, pyqtSignal, pyqt
 from PyQt6.QtGui import QPixmap, QTransform
 from PyQt6.QtWidgets import QListWidget
 
+class __Struct:
+    def __int__(self, name:str=None, avatar:str=None, ipv4:str=None, ipv6:str=None):
+        self.__name = name
+        self.__avatar = avatar
+        self.__ipv4 = ipv4
+        self.__ipv6 = ipv6
 
 class QTitlebar(QtWidgets.QWidget):
     titlebar_signal = pyqtSignal(str)
@@ -12,12 +18,17 @@ class QTitlebar(QtWidgets.QWidget):
         self.__title = title
         self._angle = [0, 180]  # 初始化角度
         self.iconpixmap = QPixmap("D:/python/CatChat/res/down.png")  # 加載圖片
+        self.setContentsMargins(0, 0, 0, 0)
 
         # 設置動畫
         self.animation = QVariantAnimation(self)
 
         # 設置界面
-        self.resize(250, 40)
+        # self.resize(250, 40)
+        # self.setMinimumSize(250, 40)
+        # self.setMaximumSize(250, 40)
+        self.setSizePolicy(QtWidgets.QSizePolicy.Policy.Expanding, QtWidgets.QSizePolicy.Policy.Expanding)
+
         self.topbarHlay = QtWidgets.QHBoxLayout(self)
         self.titleLable = QtWidgets.QLabel(self)
         self.iconlable = QtWidgets.QLabel(self)
@@ -26,7 +37,7 @@ class QTitlebar(QtWidgets.QWidget):
 
     def ui_init(self):
         # 設置標題
-        self.topbarHlay.addWidget(self.titleLable, alignment=QtCore.Qt.AlignmentFlag.AlignLeft)
+        self.topbarHlay.addWidget(self.titleLable, alignment=QtCore.Qt.AlignmentFlag.AlignLeft | QtCore.Qt.AlignmentFlag.AlignCenter)
         font = QtGui.QFont()
         font.setFamily('Arial Black')
         self.titleLable.setText(self.__title)
@@ -43,7 +54,7 @@ class QTitlebar(QtWidgets.QWidget):
         # 設置 iconlable 大小
         self.iconlable.setFixedSize(15, 15)
         self.iconlable.setAlignment(QtCore.Qt.AlignmentFlag.AlignTop)
-        self.topbarHlay.addWidget(self.iconlable, alignment=QtCore.Qt.AlignmentFlag.AlignRight)
+        self.topbarHlay.addWidget(self.iconlable, alignment=QtCore.Qt.AlignmentFlag.AlignRight | QtCore.Qt.AlignmentFlag.AlignCenter)
 
         # 初始化顯示圖片
         self.update_icon(0)
@@ -67,6 +78,22 @@ class QTitlebar(QtWidgets.QWidget):
             self.animation.valueChanged.connect(self.update_icon)  # 每次值改變時更新圖標
             self.animation.start()
 
+class QListview(QListWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+        self.setMinimumHeight(400)
+        self.setObjectName("ListW")
+
+        self.ui_init()
+
+    def ui_init(self):
+        self.setStyleSheet("""
+        #ListW {
+            color: #FFFFFF;
+            background-color: #33373B;
+        }
+        """)
+
 class QFramelessmenu(QtWidgets.QWidget):
     def __init__(self, parent=None, title: str = None):
         super().__init__(parent=parent)
@@ -75,16 +102,18 @@ class QFramelessmenu(QtWidgets.QWidget):
 
         self.Vlayout = QtWidgets.QVBoxLayout(self)
         self.__topbar = QTitlebar(parent=self, title=self.__title)
-        self.__listview = QListWidget(self)
+        self.__listview = QListview(self)
         self.ui_init()
 
     def ui_init(self):
-        self.resize(250, 250)
-        self.setMinimumSize(250, 250)
-        self.setMaximumSize(250, 800)
+        self.resize(250, 300)
+        self.setMinimumSize(250, 300)
+        self.setMaximumSize(250, 300)
         self.setSizePolicy(QtWidgets.QSizePolicy.Policy.MinimumExpanding, QtWidgets.QSizePolicy.Policy.MinimumExpanding)
 
-        self.Vlayout.addWidget(self.__topbar, alignment=QtCore.Qt.AlignmentFlag.AlignTop)
+        self.Vlayout.setSpacing(0)
+        self.Vlayout.setContentsMargins(0,0,0,0)
 
-        self.__listview.resize(200, 800)
-        self.Vlayout.addWidget(self.__listview, alignment=QtCore.Qt.AlignmentFlag.AlignBottom)
+        self.Vlayout.addWidget(self.__topbar, 0, alignment=QtCore.Qt.AlignmentFlag.AlignTop)
+
+        self.Vlayout.addWidget(self.__listview, 1, alignment=QtCore.Qt.AlignmentFlag.AlignTop)
